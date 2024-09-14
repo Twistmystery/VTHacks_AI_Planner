3a1efbe7e5cc012caa5d261d2f31694d50caddcb
Sy Traore <132951255+sytraore@users.noreply.github.com>
2024-09-14 15:23:35 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Images from './components/images';
-import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <h1>Welcome, User, to ManAzure!</h1>
       <Images></Images>
       <div>
-      <TextField id="filled-basic" label="Filled" variant="filled" />
       </div>
       <p>Suggested prompts</p>
       <p>"I have a stats test next week"</p>

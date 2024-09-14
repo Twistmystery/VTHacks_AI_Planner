c11179bf3ee3304c6946ca0353c46aaf3abe979c
Twistmystery <159201327+Twistmystery@users.noreply.github.com>
2024-09-14 15:06:38 -0400

    Added text field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Images from './components/images';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome, User, to ManAzure!</h1>
       <Images></Images>
+      <div>
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      </div>
       <p>Suggested prompts</p>
       <p>"I have a stats test next week"</p>
       <p>"Remind me to study for my bio exam tomorrow"</p>

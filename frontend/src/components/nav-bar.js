7f3c231a03d56881fa48ec8a43ac5e418e21772a
Sy Traore <132951255+sytraore@users.noreply.github.com>
2024-09-14 21:22:15 -0400

    Fixed navigation bar

--- frontend/src/components/nav-bar.js
+++ frontend/src/components/nav-bar.js
@@ -4,7 +4,9 @@ import '../index.css';
 const NavBar = (props) => {
   return (
     <nav className="navbar" style={{width: props.width}}>
-      <button class="closeButton" onClick={props.closeNav}>X</button>
+      <button class="closeButton" onClick={props.closeNav}>
+        <img className='nav-bar-icon' src='./images/menu.jpg'/>
+      </button>
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
